c00b57572c0e89b8a8ad21868f3f18b8e01b412d
quymen123 <moon01102003@gmail.com>
2025-01-20 23:31:15 +0700

    up v

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+    // pageExtensions: [
+    //     "page.ts",
+    //     "page.tsx"
+    // ],
+};
+
+export default nextConfig;
